b6b1728ced8cf288ed2b06324a62c4e42836abd5
Richard H Lee <richard@webdezign.co.uk>
2021-03-03 14:15:41 +0000

    logo animation

--- main.js
+++ main.js
@@ -1,43 +1,61 @@
-let rendering = false;
-
-let tweens = [
-  (index, page, height, box) => {
-  },
-  (index, page, height, box) => {
-    if (box.top < (height / 2)) {
-      $("video")[0].play();
-      tweens[index] = () => {};
-    }
-  },
-  (index, page, height, box) => {
-    let scroll = - box.top / height;
-    debug.innerHTML = scroll;
-    page.style.setProperty("--scroll", scroll);
-  },
-  (index, page, height, box) => {
-  }
-];
-
 $(document).ready(() => {
-  let $pages = $(".page");
+  let logos = $(".logo");
+
+  let plateauWidth = (1 / 3);
+  let plateauGradient = 1 / (1 - plateauWidth);
+  let plateauOffset = plateauWidth / (plateauWidth - 1);
+  let plateau = x => {
+    let y = 0;
+    if (x < -plateauWidth) y = (plateauGradient * x) - plateauOffset;
+    else if (x > plateauWidth) y = (plateauGradient * x) + plateauOffset;
+    return y;
+  };
+  console.log(plateau);
+
+  let stagger = -(1 / 3);
+  let middle = (logos.length - 1) / 2;
+
+  let tweens = [
+    (index, page, height, box) => {
+    },
+    (index, page, height, box) => {
+      if (box.top < (height / 2)) {
+        $("video")[0].play();
+        tweens[index] = () => {};
+      }
+    },
+    (index, page, height, box) => {
+      let scroll = - box.top / height;
+      debug.innerHTML = scroll;
+      logos.each((index, logo) => logo.style.setProperty(
+        "--scroll",
+        plateau(scroll + (stagger * (index - middle)))
+      ));
+    },
+    (index, page, height, box) => {
+    }
+  ];
+
+  let pages = $(".page");
 
   let hash = window.location.hash.slice(1);
   if (hash) {
     if (window.location.hash === "#debug") $(debug).addClass("show");
-    else $pages[parseInt(hash)].scrollIntoView();
+    else pages[parseInt(hash)].scrollIntoView();
   }
 
-  let $wrapper = $(".wrapper");
+  let wrapper = $(".wrapper");
   let scroll = () => {
-    let height = $wrapper[0].getBoundingClientRect().height;
-    $pages.each((index, page) => {
+    let height = wrapper[0].getBoundingClientRect().height;
+    pages.each((index, page) => {
       let box = page.getBoundingClientRect();
       if ((box.top < height) && (box.bottom > 0))
         tweens[index](index, page, height, box);
     });
   };
 
-  $wrapper.scroll(() => {
+  let rendering = false;
+  wrapper.scroll(() => {
     if (!rendering) {
       window.requestAnimationFrame(() => {
         scroll();
